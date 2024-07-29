550927776f298379bc53a77bceed40b98c6e6287
huanhuanwa <2323666215@qq.com>
2024-07-29 16:27:22 +0800

    fix: optmize code #WIK-16192

--- packages/grid/src/core/action/general.ts
+++ packages/grid/src/core/action/general.ts
@@ -5,44 +5,55 @@ const apply = (aiTable: AITable, records: AITableRecords, fields: AITableFields,
     switch (options.type) {
         case ActionName.UpdateFieldValue: {
             const [recordIndex, fieldIndex] = options.path;
-            const fieldId = aiTable.fields()[fieldIndex].id;
-            records[recordIndex].value[fieldId] = options.newFieldValue;
+            if (fieldIndex > -1 && recordIndex > -1) {
+                const fieldId = aiTable.fields()[fieldIndex].id;
+                records[recordIndex].value[fieldId] = options.newFieldValue;
+            }
             break;
         }
         case ActionName.AddRecord: {
             const [recordIndex] = options.path;
-            records.splice(recordIndex, 0, options.record);
+            if (recordIndex > -1) {
+                records.splice(recordIndex, 0, options.record);
+            }
             break;
         }
         case ActionName.AddField: {
             const [fieldIndex] = options.path;
-            const newField = options.field;
-            fields.splice(fieldIndex, 0, newField);
-            const newRecord = {
-                [newField.id]: ''
-            };
-            records.forEach((item) => {
-                item.value = {
-                    ...item.value,
-                    ...newRecord
+            if (fieldIndex > -1) {
+                const newField = options.field;
+                fields.splice(fieldIndex, 0, newField);
+                const newRecord = {
+                    [newField.id]: ''
                 };
-            });
+                records.forEach((item) => {
+                    item.value = {
+                        ...item.value,
+                        ...newRecord
+                    };
+                });
+            }
+
             break;
         }
         case ActionName.RemoveField: {
             const [fieldIndex] = options.path;
-            const fieldId = aiTable.fields()[fieldIndex].id;
-            fields.splice(fieldIndex, 1);
-            records.forEach((item) => {
-                delete item.value[fieldId];
-            });
+            if (fieldIndex > -1) {
+                const fieldId = aiTable.fields()[fieldIndex].id;
+                fields.splice(fieldIndex, 1);
+                records.forEach((item) => {
+                    delete item.value[fieldId];
+                });
+            }
             break;
         }
         case ActionName.RemoveRecord: {
             const [recordIndex] = options.path;
-            records.splice(recordIndex, 1);
+            if (recordIndex > -1) {
+                records.splice(recordIndex, 1);
+            }
             break;
-        }   
+        }
     }
     return {
         records,
