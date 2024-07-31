56e7fe74d36de3b07e56e9e70d161b08372d847c
pubuzhixing8 <pubuzhixing@gmail.com>
2024-07-31 16:34:48 +0800

    chore: add build script for backend and update ws url

--- src/app/share/provider.ts
+++ src/app/share/provider.ts
@@ -3,9 +3,9 @@ import * as Y from 'yjs';
 
 export const connectProvider = (doc: Y.Doc, isDev: boolean) => {
     // 在线地址：wss://demos.yjs.dev/ws
-    const prodUrl = `ws${location.protocol.slice(4)}//${location.host}`;
+    const prodUrl = `ws${location.protocol.slice(4)}//${location.host}/collaboration`;
     const devUrl = `ws${location.protocol.slice(4)}//${location.hostname}:3000`;
-    const provider = new WebsocketProvider(isDev ? devUrl : prodUrl, 'collaboration-room-1', doc);
+    const provider = new WebsocketProvider(isDev ? devUrl : prodUrl, 'room-1', doc);
     provider.connect();
     return provider;
 };
