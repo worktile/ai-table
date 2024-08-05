df1ee7bc7a8137fca79134c3a52b532d53950d5b
cmm-va <867884660@qq.com>
2024-08-05 10:39:52 +0800

    fix: 删除修改

--- src/app/component/share/share.component.ts
+++ src/app/component/share/share.component.ts
@@ -24,7 +24,7 @@ export class ShareComponent extends CommonComponent implements OnInit, AfterView
 
     provider!: WebsocketProvider | null;
 
-    room = 'room-2';
+    room = 'room-1';
 
     override ngOnInit(): void {
         const value = this.getLocalStorage();
