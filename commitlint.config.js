e445e316a14c04acd03a2590a404a0886a98a95f
huanhuanwa <2323666215@qq.com>
2024-08-20 17:14:21 +0800

    build: remove commit lint

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-angular'],
-    rules: {
-        'header-max-length': [0, 'always', 120],
-        'scope-enum': [2, 'always', ['demo', 'testing', 'grid', 'shared', 'build']],
-        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'build', 'ci', 'revert']]
-    }
-};
