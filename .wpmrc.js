92fca86f9d3163ad1c2174eb1e8fdd1b7653fd40
huanhuanwa <2323666215@qq.com>
2024-08-20 18:45:06 +0800

    build: release 0.0.10000

--- .wpmrc.js
+++ .wpmrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    allowBranch: ['develop', 'release-auto-*'],
+    allowBranch: ['develop', 'w-test', 'release-auto-*'],
     bumpFiles: ['package.json', 'package-lock.json', 'packages/grid/package.json', 'packages/shared/package.json'],
     skip: {
         confirm: true,
