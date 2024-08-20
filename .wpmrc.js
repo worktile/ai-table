dae6d7629481c5300d5709a369b47de5b9a78249
huanhuanwa <2323666215@qq.com>
2024-08-20 17:15:20 +0800

    fix: remove test in allowBranch

--- .wpmrc.js
+++ .wpmrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    allowBranch: ['develop', 'test-release', 'release-auto-*'],
+    allowBranch: ['develop', 'release-auto-*'],
     bumpFiles: ['package.json', 'package-lock.json', 'packages/grid/package.json', 'packages/shared/package.json'],
     skip: {
         confirm: true,
