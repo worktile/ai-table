1785e92110aeadcb71dfe799e91c5e264afb660e
huanhuanwa <2323666215@qq.com>
2024-08-21 09:51:17 +0800

    build: restore note

--- .wpmrc.js
+++ .wpmrc.js
@@ -4,6 +4,10 @@ module.exports = {
     skip: {
         confirm: true
     },
+    // backward compatibility changelog
+    // because we didn't use tag prefix(v) when create tag before
+    // should set tagPrefix as empty (default is 'v')
+    // otherwise, the changelog will rebuild, and will be lost past versions
     tagPrefix: '',
     hooks: {
         prepublish: 'npm run build',
