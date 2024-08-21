37df814aaa60cdd5362975175764fbe90ed3ff02
huanhuanwa <2323666215@qq.com>
2024-08-21 09:49:28 +0800

    build: adjust wpm

--- .wpmrc.js
+++ .wpmrc.js
@@ -2,14 +2,8 @@ module.exports = {
     allowBranch: ['develop', 'release-auto-*'],
     bumpFiles: ['package.json', 'package-lock.json', 'packages/grid/package.json', 'packages/shared/package.json'],
     skip: {
-        confirm: true,
-        changelog: true
+        confirm: true
     },
-    commitAll: true,
-    // backward compatibility changelog
-    // because we didn't use tag prefix(v) when create tag before
-    // should set tagPrefix as empty (default is 'v')
-    // otherwise, the changelog will rebuild, and will be lost past versions
     tagPrefix: '',
     hooks: {
         prepublish: 'npm run build',
