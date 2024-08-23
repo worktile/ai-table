226e4658a71f7efa54d10d378915f28d96be18ee
Maple13 <Mr_daijc@163.com>
2024-08-23 17:14:21 +0800

    ✨feat(grid): add dom grid renderer (#73)

--- packages/grid/src/grid-renderer/create-grid-stage.ts
+++ packages/grid/src/grid-renderer/create-grid-stage.ts
@@ -1,14 +1,14 @@
-import { Layer } from 'konva/lib/Layer';
+import Konva from 'konva/lib';
 import { AITableGridStageOptions } from '../types';
-import { Stage } from 'konva/lib/Stage';
 
 export const createGridStage = (config: AITableGridStageOptions) => {
-    const gridStage = new Stage({
+    const gridStage = new Konva.Stage({
         container: config.container,
         width: config.width,
-        height: config.height
+        height: config.height,
+        isDragging: false
     });
-    const gridLayer = new Layer();
+    const gridLayer = new Konva.Layer();
     gridStage.add(gridLayer);
 
     return gridStage;
