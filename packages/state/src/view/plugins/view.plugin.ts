3293d2d4c43bb193a4d3735c7c95c0d161fcfab0
huanhuanwa <2323666215@qq.com>
2024-09-11 16:46:17 +0800

    feat: update add record action value after filter #WIK-16464

--- packages/state/src/view/plugins/view.plugin.ts
+++ packages/state/src/view/plugins/view.plugin.ts
@@ -20,6 +20,7 @@ export const withView = (aiTable: AITable) => {
         } else if (POSITION_ACTIONS.includes(action.type as PositionActionName)) {
             GeneralPositionActions.transform(viewTable, action as AITablePositionAction);
         } else {
+            updateActionByView(viewTable, action);
             if (action.type === ActionName.AddField || action.type === ActionName.AddRecord) {
                 GeneralActions.transform(viewTable, action);
             } else {
@@ -37,3 +38,27 @@ export const withView = (aiTable: AITable) => {
     };
     return aiTable;
 };
+
+function updateActionByView(aiTable: AIViewTable, action: AITableSharedAction) {
+    const activeView = aiTable.views().find((item) => item._id === aiTable.activeViewId());
+    if (action.type === ActionName.AddRecord) {
+        if (activeView?.settings?.conditions) {
+            action.path = [aiTable.records().length];
+            // 参考 ApiTable 逻辑，当 filedId 只出现一次，并且 condition_logical 是 or 的情况下
+            // 改变新增行对应列的默认值
+            if (activeView?.settings?.conditions.length === 0 || activeView?.settings?.condition_logical === 'and') {
+                const updateFieldIdsMap: { [key: string]: any } = {};
+                activeView?.settings?.conditions.forEach((item) => {
+                    if (!updateFieldIdsMap[item.field_id]) {
+                        updateFieldIdsMap[item.field_id] = item.value;
+                    } else {
+                        delete updateFieldIdsMap[item.field_id];
+                    }
+                });
+                for (const key in updateFieldIdsMap) {
+                    action.record.values[key] = updateFieldIdsMap[key];
+                }
+            }
+        }
+    }
+}
