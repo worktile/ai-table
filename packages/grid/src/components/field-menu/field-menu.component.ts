588f644bac1385b1ccadfff6915598f1842d56a6
Maple13 <Mr_daijc@163.com>
2024-09-20 11:08:20 +0800

    ✨feat(grid): support opening the attribute column menu (#103)

--- packages/grid/src/components/field-menu/field-menu.component.ts
+++ packages/grid/src/components/field-menu/field-menu.component.ts
@@ -1,14 +1,15 @@
-import { Component, ChangeDetectionStrategy, Input, ElementRef, computed } from '@angular/core';
-import { AITableFieldMenuItem } from '../../types/field';
-import { AITable } from '../../core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, computed } from '@angular/core';
+import { ThyDivider } from 'ngx-tethys/divider';
 import {
+    ThyDropdownAbstractMenu,
+    ThyDropdownMenuComponent,
     ThyDropdownMenuItemDirective,
-    ThyDropdownMenuItemNameDirective,
     ThyDropdownMenuItemIconDirective,
-    ThyDropdownMenuComponent
+    ThyDropdownMenuItemNameDirective
 } from 'ngx-tethys/dropdown';
 import { ThyIcon } from 'ngx-tethys/icon';
-import { ThyDivider } from 'ngx-tethys/divider';
+import { AITable } from '../../core';
+import { AITableFieldMenuItem } from '../../types/field';
 
 @Component({
     selector: 'field-menu',
@@ -24,7 +25,7 @@ import { ThyDivider } from 'ngx-tethys/divider';
         ThyDropdownMenuItemIconDirective
     ]
 })
-export class FieldMenu {
+export class FieldMenu extends ThyDropdownAbstractMenu {
     @Input({ required: true }) fieldId!: string;
 
     @Input({ required: true }) aiTable!: AITable;
@@ -33,13 +34,15 @@ export class FieldMenu {
 
     @Input() origin!: HTMLElement | ElementRef<any>;
 
+    @Input() position!: { x: number; y: number };
+
     field = computed(() => {
         return this.aiTable.fields().find((item) => item._id === this.fieldId)!;
     });
 
     execute(menu: AITableFieldMenuItem) {
         if ((menu.disabled && !menu.disabled(this.aiTable, this.field)) || !menu.disabled) {
-            menu.exec && menu.exec(this.aiTable, this.field, this.origin);
+            menu.exec && menu.exec(this.aiTable, this.field, this.origin, this.position);
         }
     }
 }
--- packages/grid/src/types/field.ts
+++ packages/grid/src/types/field.ts
@@ -5,11 +5,32 @@ export interface AITableFieldMenuItem {
     type: string;
     name?: string;
     icon?: string;
-    exec?: (aiTable: AITable, field: Signal<AITableField>, origin?: HTMLElement | ElementRef<any>) => void;
+    exec?: (
+        aiTable: AITable,
+        field: Signal<AITableField>,
+        origin?: HTMLElement | ElementRef<any>,
+        position?: { x: number; y: number }
+    ) => void;
     hidden?: (aiTable: AITable, field: Signal<AITableField>) => boolean;
     disabled?: (aiTable: AITable, field: Signal<AITableField>) => boolean;
 }
 
 export interface AITableSelectField extends AITableField {
     settings: SelectSettings;
+}
+
+export interface AITableEditFieldOptions {
+    field: Signal<AITableField>;
+    isUpdate: boolean;
+    origin: HTMLElement | ElementRef<any>;
+    position?: { x: number; y: number };
+}
+
+export interface AITableFieldMenuOptions {
+    fieldId: string;
+    fieldMenus: AITableFieldMenuItem[];
+    editOrigin?: any;
+    origin?: any;
+    position?: { x: number; y: number };
+    editOriginPosition?: { x: number; y: number };
 }
