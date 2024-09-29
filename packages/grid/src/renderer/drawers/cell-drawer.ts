d3776f8f79fce550c4de9c90b0530d5bbddac00a
cmm-va <33084123+cmm-va@users.noreply.github.com>
2024-09-29 10:10:13 +0800

    fix: adjust  drawable index (#122)

--- packages/grid/src/renderer/drawers/cell-drawer.ts
+++ packages/grid/src/renderer/drawers/cell-drawer.ts
@@ -178,22 +178,33 @@ export class CellDrawer extends Drawer {
             if (totalWidth < maxContainerWidth || totalWidth === maxContainerWidth) {
                 drawableIndex = index;
             }
-
             cellItemInfoMap.set(optionId, { textWidth, item, offset: totalWidth });
         });
 
         const baseWidth = AI_TABLE_MIN_TEXT_WIDTH + AI_TABLE_CELL_PADDING * 2;
         const minWidth = isDotOrPiece ? baseWidth + AI_TABLE_CELL_MULTI_DOT_RADIUS * 2 + AI_TABLE_CELL_MULTI_PADDING_LEFT : baseWidth;
+ 
+
         if (transformValue[drawableIndex + 1]) {
-            const { offset: offsetWidth } = cellItemInfoMap.get(transformValue[drawableIndex]);
-            const { offset: nextOffset } = cellItemInfoMap.get(transformValue[drawableIndex + 1]);
-            drawableIndex =
-                maxContainerWidth - offsetWidth > minWidth && nextOffset > maxContainerWidth ? drawableIndex + 1 : drawableIndex;
-            const number = transformValue.length - (drawableIndex + 1);
+            const { offset: currentOffset } = cellItemInfoMap.get(cellValue[drawableIndex]);
+            const canDrawerNext = maxContainerWidth - currentOffset > minWidth;
+            drawableIndex = canDrawerNext ? drawableIndex + 1 : drawableIndex;
+            // 上面过程是  没有 +数字  的情况下最大能放几个；
+            const number = cellValue.length - (drawableIndex + 1);
+ 
             if (number > 0) {
+                // 说明有 +数字，重新计算
                 const circleWidth = getTextWidth(ctx, `+{number}`, fontStyle) + 2 * AI_TABLE_CELL_PADDING;
                 const max = maxContainerWidth - AI_TABLE_CELL_MULTI_PADDING_LEFT - circleWidth;
-                drawableIndex = max - offsetWidth > minWidth && nextOffset > max ? drawableIndex : drawableIndex - 1;
+                // 如果当前已经超出了，看是否能容下当前的，不能就减去  1；
+                const currentItemHasOver = currentOffset > max;
+                if (currentItemHasOver) {
+                    const lastOffset = drawableIndex === 0 ? 0 : cellItemInfoMap.get(cellValue[drawableIndex - 1]);
+                    drawableIndex = max - lastOffset > minWidth ? drawableIndex : drawableIndex - 1;
+                } else {
+                    // 还有剩余空间, 看是否能多渲染一个
+                    drawableIndex = max - currentOffset > minWidth ? drawableIndex + 1 : drawableIndex;
+                }
             }
         }
 
