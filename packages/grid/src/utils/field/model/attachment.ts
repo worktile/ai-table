3ceb0209583cf149abc97e5b5afd22b6e936ba3e
wumeimin0107 <321512939@qq.com>
2025-03-24 11:02:10 +0800

    feat(grid): support paste link field and dont support paste attachment (#279)
    * feat(grid): support paste link field
    
    * feat(grid): dont support paste attachment

--- packages/grid/src/utils/field/model/attachment.ts
+++ packages/grid/src/utils/field/model/attachment.ts
@@ -1,8 +1,7 @@
-import { Id } from 'ngx-tethys/types';
-import { AITableField, AITableFieldType, AttachmentFieldValue, FieldValue } from '../../../core';
+import { AITableField, AttachmentFieldValue, FieldValue } from '../../../core';
 import { AITableFilterCondition, AITableFilterOperation, AITableReferences } from '../../../types';
 import { isEmpty } from '../../common';
-import { compareString, hasIntersect, stringInclude } from '../operate';
+import { compareString, hasIntersect } from '../operate';
 import { Field } from './field';
 
 export class AttachmentField extends Field {
@@ -65,39 +64,6 @@ export function toAttachmentFieldValue(
     originData?: { field: AITableField; cellValue: AttachmentFieldValue },
     references?: AITableReferences
 ): FieldValue | null {
-    if (targetField.type == AITableFieldType.createdBy || targetField.type == AITableFieldType.updatedBy) {
-        return null;
-    }
-    if (originData) {
-        const { field, cellValue } = originData;
-        switch (field.type) {
-            case AITableFieldType.attachment:
-                if (Array.isArray(cellValue) && cellValue.length) {
-                    return cellValue;
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    plainText = plainText.trim();
-    const hasAttachmentInfo = references && references.attachments && Object.keys(references.attachments).length;
-    if (plainText && hasAttachmentInfo) {
-        const attachmentTitles = plainText.split(',').map((id) => id.trim());
-        const attachmentInfos = Object.values(references.attachments);
-        let validAttachmentIds: AttachmentFieldValue = [];
-        attachmentTitles.forEach((fileTitle) => {
-            const attachmentInfo = attachmentInfos.find((attachment) => attachment.title === fileTitle);
-            if (attachmentInfo) {
-                validAttachmentIds.push(attachmentInfo._id as string);
-            }
-        });
-        if (validAttachmentIds.length) {
-            return validAttachmentIds;
-        }
-    }
-
     return null;
 }
 
