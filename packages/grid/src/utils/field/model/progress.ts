3e27787412405b43d7ec605bbf13e45acf95e05f
wumeimin0107 <321512939@qq.com>
2025-03-26 16:39:45 +0800

    feat(grid): support paste to rate and progress (#284)

--- packages/grid/src/utils/field/model/progress.ts
+++ packages/grid/src/utils/field/model/progress.ts
@@ -54,34 +54,32 @@ export function toProgressFieldValue(
     targetField: AITableField,
     originData?: { field: AITableField; cellValue: FieldValue }
 ): FieldValue | null {
-    //  进度支持选中单元格后、验证、再提交
+    let value: any = plainText.trim();
+    if (originData) {
+        const { field, cellValue } = originData;
+        switch (field.type) {
+            case AITableFieldType.progress:
+            case AITableFieldType.rate:
+            case AITableFieldType.number:
+                value = cellValue;
+                break;
+            case AITableFieldType.select:
+                if (cellValue && Array.isArray(cellValue) && cellValue.length) {
+                    const optionsMap = helpers.keyBy((field.settings as SelectSettings).options || [], '_id');
+                    value = optionsMap[cellValue[0]]?.text;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
-    // let value: any = plainText.trim();
-    // if (originData) {
-    //     const { field, cellValue } = originData;
-    //     switch (field.type) {
-    //         case AITableFieldType.progress:
-    //         case AITableFieldType.rate:
-    //         case AITableFieldType.number:
-    //             value = cellValue;
-    //             break;
-    //         case AITableFieldType.select:
-    //             if (cellValue && Array.isArray(cellValue) && cellValue.length) {
-    //                 const optionsMap = helpers.keyBy((field.settings as SelectSettings).options || [], '_id');
-    //                 value = optionsMap[cellValue[0]]?.text;
-    //             }
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
-
-    // if (!isEmpty(value)) {
-    //     const progressValue = Number(value);
-    //     if (!Number.isNaN(progressValue) && progressValue >= 0 && progressValue <= 100) {
-    //         return progressValue;
-    //     }
-    // }
+    if (!isEmpty(value)) {
+        const progressValue = Number(value);
+        if (!Number.isNaN(progressValue) && progressValue >= 0 && progressValue <= 100) {
+            return progressValue;
+        }
+    }
 
     return null;
 }
