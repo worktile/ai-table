9f60d96ccf6f2056adb06d5594419778bbb4cd81
icezeros <icezeros@163.com>
2025-04-09 16:48:40 +0800

    feat: #WIK-17533 【aitable】支持 readonly 下也需要抛出事件 (#329)

--- packages/grid/src/grid.component.ts
+++ packages/grid/src/grid.component.ts
@@ -378,9 +378,15 @@ export class AITableGrid extends AITableGridBase implements OnInit, OnDestroy {
     }
 
     stageClick(e: KoEventObject<MouseEvent>) {
+        const targetNameDetail = getDetailByTargetName(e.event.target.name());
+        this.aiClick.emit({
+            ...e,
+            targetNameDetail
+        });
         const mouseEvent = e.event.evt;
         mouseEvent.preventDefault();
         this.aiTableGridEventService.closeCellEditor();
+
         const { context } = this.aiTable;
         const { targetName, rowIndex: pointRowIndex } = context!.pointPosition();
         if (mouseEvent.button !== AITableMouseDownType.Left || (targetName !== AI_TABLE_FIELD_HEAD_MORE && this.aiReadonly())) return;
@@ -440,25 +446,17 @@ export class AITableGrid extends AITableGridBase implements OnInit, OnDestroy {
                 }
                 break;
         }
-        const targetNameDetail = getDetailByTargetName(e.event.target.name());
-
-        this.aiClick.emit({
-            ...e,
-            targetNameDetail
-        });
         return;
     }
 
     stageDblclick(e: KoEventObject<MouseEvent>) {
         const _targetName = e.event.target.name();
         const targetNameDetail = getDetailByTargetName(_targetName);
-        if (this.aiReadonly()) {
-            this.aiDbClick.emit({
-                ...e,
-                targetNameDetail
-            });
-            return;
-        }
+        this.aiDbClick.emit({
+            ...e,
+            targetNameDetail
+        });
+
         const { fieldId, recordId } = targetNameDetail;
         if (!recordId || !fieldId) {
             return;
@@ -479,11 +477,6 @@ export class AITableGrid extends AITableGridBase implements OnInit, OnDestroy {
                     }
                 });
             }, 0);
-        } else {
-            this.aiDbClick.emit({
-                ...e,
-                targetNameDetail
-            });
         }
     }
 
