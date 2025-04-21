d1319d9eb253530913c5754cd641e7a49a6f6046
minlovehua <321512939@qq.com>
2025-04-21 11:44:53 +0800

    feat(grid): use canvas technology to render progress

--- packages/grid/src/renderer/drawers/cell-drawer.ts
+++ packages/grid/src/renderer/drawers/cell-drawer.ts
@@ -31,7 +31,7 @@ import {
     AI_TABLE_PIECE_WIDTH,
     AI_TABLE_PROGRESS_BAR_HEIGHT,
     AI_TABLE_PROGRESS_BAR_RADIUS,
-    AI_TABLE_PROGRESS_TEXT_Width,
+    AI_TABLE_PROGRESS_TEXT_WIDTH,
     AI_TABLE_ROW_BLANK_HEIGHT,
     AI_TABLE_TAG_FONT_SIZE,
     AI_TABLE_TAG_PADDING,
@@ -553,7 +553,7 @@ export class CellDrawer extends Drawer {
         if (!_.isNumber(cellValue)) {
             return;
         }
-        const width = columnWidth - 2 * AI_TABLE_CELL_PADDING - AI_TABLE_PROGRESS_TEXT_Width;
+        const width = columnWidth - 2 * AI_TABLE_CELL_PADDING - AI_TABLE_PROGRESS_TEXT_WIDTH;
         const height = AI_TABLE_PROGRESS_BAR_HEIGHT;
         const textHeight = AI_TABLE_COMMON_FONT_SIZE;
         const offsetX = AI_TABLE_CELL_PADDING;
