f1425c9e41fa035496599f45c47fe48f4bff9281
minlovehua <321512939@qq.com>
2025-04-21 17:20:10 +0800

    refactor(grid): rename richtext to rich-text for file

--- packages/grid/src/renderer/components/cells/index.ts
+++ packages/grid/src/renderer/components/cells/index.ts
@@ -1,5 +1,5 @@
 // TODO: components下的cells组件考虑移出 components ，这些组件都属于
 export * from './link.component';
 export * from './attachment.component';
-export * from './richtext.component';
+export * from './rich-text.component';
 export * from './rate.component';
