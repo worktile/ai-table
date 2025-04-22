5bdf70e6707b40db6f2fc89da16d030e2e596e4b
Xwatson <xwatson@foxmail.com>
2025-04-22 16:30:04 +0800

    fix(drag): #WIK-17762 scrolling and drag position calculation problem (#376)

--- packages/grid/src/components/drag/drag.component.ts
+++ packages/grid/src/components/drag/drag.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, effect, ElementRef, inject, OnDestr
 import { AITableDragState, DragEndData, DragType } from '../../core';
 import { AITableGridSelectionService } from '../../services/selection.service';
 import { MIN_COLUMN_WIDTH } from '../../constants/grid';
-import { AI_TABLE_ROW_DRAG_ICON_WIDTH } from '../../constants/table';
+import { AI_TABLE_FIELD_HEAD_HEIGHT, AI_TABLE_ROW_DRAG_ICON_WIDTH } from '../../constants/table';
 
 @Component({
     selector: 'ai-table-drag',
@@ -226,24 +226,28 @@ export class AITableDragComponent implements OnInit, OnDestroy {
         const sourceRowIndex = visibleRowIndexMap.get(sourceRowId) || 0;
         const sourceRowStartY = coordinate.getRowOffset(sourceRowIndex);
         const sourceRowHeight = coordinate.getRowHeight(sourceRowIndex);
+        const pointerY = sourceRowStartY + moveY;
         this.setRectStyles({
             width: '100%',
             height: `${sourceRowHeight}px`,
-            top: `${sourceRowStartY + moveY}px`,
+            top: `${pointerY - scroll.y}px`,
             left: '0'
         });
-
-        const pointerY = moveY + sourceRowStartY + sourceRowHeight / 2;
-        const targetRowIndex = coordinate.getRowStartIndex(pointerY + scroll.y);
+        const dragCenter = sourceRowHeight / 2;
+        const targetRowIndex = coordinate.getRowStartIndex(pointerY + dragCenter);
         const targetRowStartY = coordinate.getRowOffset(targetRowIndex);
+        const lineTop = targetRowStartY - scroll.y;
+        const lineHeight = 2;
         if (
-            (targetRowIndex >= 0 && sourceRowIndex > targetRowIndex && sourceRowIndex - targetRowIndex > 0) ||
-            (sourceRowIndex < targetRowIndex && targetRowIndex - sourceRowIndex > 1)
+            ((targetRowIndex >= 0 && sourceRowIndex > targetRowIndex && sourceRowIndex - targetRowIndex > 0) ||
+                (sourceRowIndex < targetRowIndex && targetRowIndex - sourceRowIndex > 1)) &&
+            lineTop > AI_TABLE_FIELD_HEAD_HEIGHT - lineHeight && // 限制可视范围内
+            lineTop < coordinate.containerHeight - lineHeight
         ) {
             this.setAuxiliaryLineStyles({
                 width: `calc(100% - ${AI_TABLE_ROW_DRAG_ICON_WIDTH}px)`,
-                height: '2px',
-                top: `${targetRowStartY}px`,
+                height: `${lineHeight}px`,
+                top: `${lineTop}px`,
                 left: `${AI_TABLE_ROW_DRAG_ICON_WIDTH}px`
             });
             this.draggedData = {
