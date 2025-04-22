065809151f97d8ec567d62a57dfbfb2a6dd78ae2
icezeros <icezeros@163.com>
2025-04-22 11:38:04 +0800

    feat: #WIK-17761 【AiTable】筛选条件下，新增数据交互

--- packages/grid/src/grid.component.ts
+++ packages/grid/src/grid.component.ts
@@ -197,7 +197,7 @@ export class AITableGrid extends AITableGridBase implements OnInit, OnDestroy {
                 // 当新增行选中的cell,编辑后，activeCell 不在新增的行中时，根据筛选 过滤行数据,触发重新渲染
                 const activeCell = this.aiTable.selection().activeCell;
                 untracked(() => {
-                    if (!activeCell || !this.aiTable.recordsWillHidden().includes(activeCell[0])) {
+                    if (this.addActiveStatus && (!activeCell || !this.aiTable.recordsWillHidden().includes(activeCell[0]))) {
                         this.addActiveStatus = false;
                         this.aiTable.recordsWillHidden.set([]);
                         this.refreshRender.set(this.refreshRender() + 1);
