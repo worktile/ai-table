0ec14f76997e8bb4ed38f37d63782d0af6458dfd
icezeros <icezeros@163.com>
2025-04-23 15:47:25 +0800

    fix: #WIK-17804 附件很多的时候拉宽后，换行

--- packages/grid/src/renderer/drawers/cell-drawer.ts
+++ packages/grid/src/renderer/drawers/cell-drawer.ts
@@ -720,14 +720,11 @@ export class CellDrawer extends Drawer {
         let currentX = AI_TABLE_CELL_PADDING;
         let currentY = (AI_TABLE_ROW_BLANK_HEIGHT - itemHeight) / 2;
         const itemOtherWidth = fileIconSize + AI_TABLE_FIELD_ITEM_MARGIN_RIGHT;
-        const maxHeight = isActive ? 130 - AI_TABLE_CELL_MULTI_PADDING_TOP : rowHeight - AI_TABLE_CELL_MULTI_PADDING_TOP;
         const maxTextWidth = isOperating
             ? columnWidth - 2 * AI_TABLE_CELL_PADDING - itemOtherWidth - AI_TABLE_CELL_DELETE_ITEM_BUTTON_SIZE - 12
             : columnWidth - 2 * AI_TABLE_CELL_PADDING - itemOtherWidth;
 
         const listCount = transformValue.length;
-        let isOverflow = false;
-
         for (let index = 0; index < listCount; index++) {
             const attachmentInfo = references?.attachments[transformValue[index]];
             if (!attachmentInfo) continue;
@@ -737,29 +734,12 @@ export class CellDrawer extends Drawer {
             let realMaxTextWidth = maxTextWidth < 0 ? 0 : maxTextWidth;
             if (index === 0 && isOperating) {
                 const operatingMaxWidth = maxTextWidth - (AI_TABLE_CELL_ADD_ITEM_BUTTON_SIZE + 4);
-                // item No space to display, then perform a line feed
-                if (operatingMaxWidth <= 20) {
-                    currentX = AI_TABLE_CELL_PADDING;
-                    currentY += AI_TABLE_OPTION_ITEM_HEIGHT + AI_TABLE_CELL_MULTI_ITEM_MARGIN_TOP;
-                } else {
-                    realMaxTextWidth = operatingMaxWidth;
-                }
+                realMaxTextWidth = operatingMaxWidth;
             }
             if (columnWidth != null) {
                 // 在非活动状态下，当超出列宽时，不会渲染后续内容
                 if (currentX >= columnWidth - 2 * AI_TABLE_CELL_PADDING) {
                     break;
-                }
-                // 如果不是非活动状态的最后一行，则换行渲染溢出内容
-                if (currentX > columnWidth - 2 * AI_TABLE_CELL_PADDING) {
-                    currentX = AI_TABLE_CELL_PADDING;
-                }
-                if (currentX + itemWidth > columnWidth - AI_TABLE_CELL_PADDING) {
-                    currentX = AI_TABLE_CELL_PADDING;
-                    currentY += itemHeight;
-                }
-                if (currentY >= maxHeight) {
-                    isOverflow = true;
                 }
             }
             const svgString = getFileThumbnailSvgString(addition?.ext);
