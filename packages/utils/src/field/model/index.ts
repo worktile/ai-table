ef8b1706819e84ee4fb00f8315048c7bd6a0f570
icezeros <icezeros@163.com>
2025-04-24 15:58:54 +0800

    feat: #WIK-17779 【Utils】确定 field-model 复用方案（isValid + cellFullText）

--- packages/utils/src/field/model/index.ts
+++ packages/utils/src/field/model/index.ts
@@ -1,44 +1,11 @@
-import { AITableFieldType } from '../../types';
-import { AttachmentFieldBase } from './attachment';
-import { DateFieldBase } from './date';
-import { FieldBase } from './field';
-
-import { LinkFieldBase } from './link';
-import { MemberFieldBase } from './member';
-import { NumberFieldBase } from './number';
-import { ProgressFieldBase } from './progress';
-import { RateFieldBase } from './rate';
-import { RichTextFieldBase } from './rich-text';
-import { SelectFieldBase } from './select';
-import { TextFieldBase } from './text';
-
-const FieldModelMap: Record<AITableFieldType, FieldBase> = {
-    [AITableFieldType.text]: new TextFieldBase(),
-    [AITableFieldType.richText]: new RichTextFieldBase(),
-    [AITableFieldType.select]: new SelectFieldBase(),
-    [AITableFieldType.date]: new DateFieldBase(),
-    [AITableFieldType.createdAt]: new DateFieldBase(),
-    [AITableFieldType.updatedAt]: new DateFieldBase(),
-    [AITableFieldType.number]: new NumberFieldBase(),
-    [AITableFieldType.rate]: new RateFieldBase(),
-    [AITableFieldType.link]: new LinkFieldBase(),
-    [AITableFieldType.member]: new MemberFieldBase(),
-    [AITableFieldType.progress]: new ProgressFieldBase(),
-    [AITableFieldType.createdBy]: new MemberFieldBase(),
-    [AITableFieldType.updatedBy]: new MemberFieldBase(),
-    [AITableFieldType.attachment]: new AttachmentFieldBase()
-};
-export {
-    FieldModelMap,
-    AttachmentFieldBase,
-    DateFieldBase,
-    FieldBase,
-    LinkFieldBase,
-    MemberFieldBase,
-    NumberFieldBase,
-    ProgressFieldBase,
-    RateFieldBase,
-    RichTextFieldBase,
-    SelectFieldBase,
-    TextFieldBase
-};
+export * from './attachment';
+export * from './date';
+export * from './field';
+export * from './link';
+export * from './member';
+export * from './number';
+export * from './progress';
+export * from './rate';
+export * from './rich-text';
+export * from './select';
+export * from './text';
--- packages/utils/src/public-api.ts
+++ packages/utils/src/public-api.ts
@@ -1,4 +1,4 @@
 export * from './constants';
 export * from './types';
 export * from './utils';
-export * from './field/model';
+export * from './field';
