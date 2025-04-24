c2c6644829051ac35f8bbc300c01bbafd933593b
xwatson <xwatson@foxmail.com>
2025-04-24 15:15:04 +0800

    fix(state): #WIK-17815 new field should remove position

--- packages/state/src/utils/field/add-fields.ts
+++ packages/state/src/utils/field/add-fields.ts
@@ -7,14 +7,14 @@ import { updateRecordsUpdatedInfo } from '../record/update-system-field-value';
 export function addFields(aiTable: AIViewTable, options: AddFieldOptions, updatedInfo: AITableRecordUpdatedInfo) {
     const { originId, direction = Direction.after, defaultValue, isDuplicate, count = 1 } = options;
     const activeView = aiTable.viewsMap()[aiTable.activeViewId()];
-    const fields = getSortFields(aiTable, aiTable.fields() as AITableViewFields, activeView);
+    const fields = getSortFields(aiTable, aiTable.gridData().fields as AITableViewFields, activeView);
     let addIndex = fields.findIndex((item) => item._id === originId);
     if (direction === Direction.after) {
         addIndex++;
     }
     const newFieldIds = idsCreator(count);
     newFieldIds.forEach((id, index) => {
-        const newField = { _id: id, ...defaultValue } as AITableField;
+        const newField = { _id: id, ...defaultValue, positions: null } as AITableField;
         Actions.addField(aiTable, newField, [addIndex + index], options.originId, options.isCopy);
     });
     updateRecordsUpdatedInfo(aiTable, updatedInfo);
