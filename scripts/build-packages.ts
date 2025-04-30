b5e1e9e324abed11ff42e262ff1a65d91ff1f15a
icezeros <icezeros@163.com>
2025-04-30 10:09:09 +0800

    feat: #WIK-17780 前端改造接入 utils 库

--- scripts/build-packages.ts
+++ scripts/build-packages.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
 import * as path from 'path';
-import * as fs from 'fs';
 import * as ngPackage from 'ng-packagr';
 
-// const packagesPath = path.resolve(__dirname, '../packages');
-// const packages = fs.readdirSync(packagesPath).filter((name) => fs.statSync(path.resolve(packagesPath, name)).isDirectory());
 const packages = ['utils', 'grid', 'state'];
 
 async function buildNgPackages() {
@@ -21,19 +18,6 @@ async function buildNgPackages() {
                 console.error(error);
                 process.exit(1);
             });
-        // console.log(`Building ${pkg}...`);
-        // const target = path.resolve(__dirname, `../packages/${pkg}`);
-        // try {
-        //     await ngPackage
-        //         .ngPackagr()
-        //         .forProject(path.resolve(target, 'ng-package.json'))
-        //         .withTsConfig(path.resolve(target, 'tsconfig.lib.json'))
-        //         .build();
-        //     console.log(`Successfully built ${pkg}`);
-        // } catch (error) {
-        //     console.error(`Error building ${pkg}:`, error);
-        //     process.exit(1);
-        // }
     }
 }
 
