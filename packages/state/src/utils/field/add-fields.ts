55aa3edb7801ed5eea9c3872328c15c7e787df8f
icezeros <icezeros@163.com>
2025-05-22 14:07:50 +0800

    feat: #WIK-18019 列的更多菜单「重复」列，位置不正确

--- packages/state/src/utils/field/add-fields.ts
+++ packages/state/src/utils/field/add-fields.ts
@@ -18,7 +18,7 @@ export function addFields(aiTable: AIViewTable, options: AddFieldOptions) {
         const currentFieldIndex = fields.findIndex((item) => item._id === originId);
         newField.positions = {
             ...newField.positions,
-            [activeViewId]: getPosition(fields, activeViewId, currentFieldIndex - 1)
+            [activeViewId]: getPosition(fields, activeViewId, currentFieldIndex)
         };
     } else {
         newField.positions = createPositions(
