27fe6221289cb1d4252a9094001b229cdfbfc81b
why520crazy <why520crazy@users.noreply.github.com>
2025-05-23 10:47:08 +0800

    docs: add basic example (#484)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { DemoTableContent } from './component/common/content/content.component';
 import { DemoTable } from './component/table.component';
+import { BasicTableExample } from './basic/basic.component';
 
 export const routes: Routes = [
+    {
+        path: 'basic',
+        component: BasicTableExample
+    },
     {
         path: '',
         component: DemoTable,
