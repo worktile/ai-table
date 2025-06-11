a2ad47a7d59c8994cbf0d0978cd134c00ec25712
minlovehua <321512939@qq.com>
2025-06-11 18:44:41 +0800

    feat(utils): add generateOptionsByTexts function

--- packages/utils/src/field/model/select.ts
+++ packages/utils/src/field/model/select.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { AITableField, SelectFieldValue, SelectSettings } from '../../types';
 import { FieldBase } from './field';
-import { keyBy } from '../../helps';
+import { isUndefinedOrNull, keyBy, idCreator, DEFAULT_COLORS } from '../../helps';
 
 export class SelectFieldBase extends FieldBase {
     override isValid(cellValue: SelectFieldValue): boolean {
@@ -21,4 +21,21 @@ export class SelectFieldBase extends FieldBase {
         }
         return fullText;
     }
+}
+
+export function generateOptionsByTexts(texts: string[]) {
+    texts = texts.filter((value) => {
+        return !isUndefinedOrNull(value) && value !== '';
+    });
+    texts = _.uniq(texts);
+
+    const options = texts.map((value) => {
+        const option = {
+            _id: idCreator(),
+            text: value,
+            bg_color: DEFAULT_COLORS[10 + (texts.length || 0)]
+        };
+        return option;
+    });
+    return options;
 }
