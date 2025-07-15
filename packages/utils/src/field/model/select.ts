4c2839f92763f11cc132e2f1a427bcdfedb5ce88
icezeros <icezeros@163.com>
2025-07-15 16:46:00 +0800

    fix: #WIK-18465 【统计行】多选统计指标不全，缺少唯一数和唯一数占比 (#572)

--- packages/utils/src/field/model/select.ts
+++ packages/utils/src/field/model/select.ts
@@ -5,16 +5,6 @@ import { isUndefinedOrNull, keyBy, idCreator, DEFAULT_COLORS } from '../../helps
 import { DEFAULT_FIELD_STAT_TYPE_MAP } from '../../constants';
 
 export class SelectFieldBase extends FieldBase {
-    constructor() {
-        super([
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.None]!,
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.CountAll]!,
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.Filled]!,
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.Empty]!,
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.PercentFilled]!,
-            DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.PercentEmpty]!
-        ]);
-    }
     override isValid(cellValue: SelectFieldValue): boolean {
         return Array.isArray(cellValue) || cellValue === null;
     }
