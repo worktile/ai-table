01d516ca7dbfa5b27f9237d8b723088d01eebb51
icezeros <icezeros@163.com>
2025-07-17 14:42:20 +0800

    fix: #WIK-18468 【统计行】数字求和达到亿要简化显示，参考组件

---
+++ packages/utils/src/helps/number.ts
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+
+export function numberFormat(num: number, precision: number = 2) {
+    if (!_.isFinite(num)) return _.toString(num);
+    const absNum = Math.abs(num);
+    if (absNum < 1e8) {
+        const str = _.toString(num);
+        const [intPart, decPart] = _.split(str, '.');
+        return decPart ? intPart + '.' + decPart.slice(0, precision).replace(/0+$/, '') : intPart;
+    }
+
+    const [base, exp] = _.split(num.toExponential(), 'e');
+    const [integer, decimal] = _.split(base, '.');
+    return `${integer}${decimal ? '.' + decimal : ''}e${exp}`;
+}
