4a2ce6fbc52c04371b1f380c0e287933029ecab0
icezeros <icezeros@163.com>
2025-07-25 14:59:55 +0800

    feat: #WIK-18599 AITable-复选框统计调整 (#594)

--- packages/utils/src/field/model/checkbox.ts
+++ packages/utils/src/field/model/checkbox.ts
@@ -5,7 +5,16 @@ import _ from 'lodash';
 
 export class CheckboxFieldBase extends FieldBase {
     constructor(statTypes?: AITableFieldStatTypeItemInfo[]) {
-        super(statTypes || DEFAULT_FIELD_STAT_TYPE_ITEMS);
+        super(
+            statTypes || [
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.None]!,
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.CountAll]!,
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.Checked]!,
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.UnChecked]!,
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.PercentChecked]!,
+                DEFAULT_FIELD_STAT_TYPE_MAP[AITableStatType.PercentUnChecked]!
+            ]
+        );
     }
 
     override isValid(cellValue: DateFieldValue): boolean {
