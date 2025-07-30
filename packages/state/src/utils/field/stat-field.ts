fbba8bd9135fd7355e2174def0f8d59903fb5795
icezeros <icezeros@163.com>
2025-07-30 17:43:31 +0800

    fix: #WIK-18612 【aitable】表格又出现白屏，出现多次

--- packages/state/src/utils/field/stat-field.ts
+++ packages/state/src/utils/field/stat-field.ts
@@ -1,13 +1,15 @@
 import { AITableView, AITableViewFields } from '@ai-table/utils';
 
 export function buildFieldStatType(data: AITableViewFields, activeView: AITableView) {
-    data.forEach((field, index) => {
+    return data.map((field, index) => {
         const fieldStatType = field.fieldStatTypes?.[activeView._id];
         if (fieldStatType) {
-            data[index] = {
+            return {
                 ...field,
                 stat_type: fieldStatType
             };
+        } else {
+            return field;
         }
     });
 }
