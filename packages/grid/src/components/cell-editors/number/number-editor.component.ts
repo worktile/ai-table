5e9da47da4c70f391bc5458d98c3183d7007d248
Xwatson <xwatson@foxmail.com>
2025-08-01 16:21:56 +0800

    fix(state): #WIK-18472 数字类型清除回车后数字还原 (#609)

--- packages/grid/src/components/cell-editors/number/number-editor.component.ts
+++ packages/grid/src/components/cell-editors/number/number-editor.component.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { ThyInputNumber } from 'ngx-tethys/input-number';
 import { ThyEnterDirective } from 'ngx-tethys/shared';
 import { AbstractEditCellEditor } from '../abstract-cell-editor.component';
+import { FieldModelMap } from '../../../utils';
+import { NumberFieldValue } from '@ai-table/utils';
 
 @Component({
     selector: 'number-cell-editor',
@@ -19,8 +21,12 @@ import { AbstractEditCellEditor } from '../abstract-cell-editor.component';
         class: 'number-cell-editor'
     }
 })
-export class NumberCellEditorComponent extends AbstractEditCellEditor<number> {
+export class NumberCellEditorComponent extends AbstractEditCellEditor<NumberFieldValue> {
     updateValue() {
+        if ((this.modelValue as unknown as string) === '') {
+            const fieldModel = FieldModelMap[this.field().type];
+            this.modelValue = fieldModel.transformCellValue(this.modelValue, { aiTable: this.aiTable, field: this.field() });
+        }
         super.update();
         this.closePopover();
     }
