b7374f0530ff7fe04e69a107d287eb77366b91c6
icezeros <icezeros@163.com>
2025-08-14 14:40:10 +0800

    fix: #WIK-18711 【复选框】同类型粘贴至复制框应该可以成功 (#626)

--- packages/grid/src/utils/field/model/checkbox.ts
+++ packages/grid/src/utils/field/model/checkbox.ts
@@ -57,6 +57,12 @@ export class CheckboxField extends CheckboxFieldBase implements FieldOperable<st
         targetField: AITableField,
         originData?: { field: AITableField; cellValue: FieldValue }
     ): FieldValue | null {
+        if (originData) {
+            const { field, cellValue } = originData;
+            if (field.type === AITableFieldType.checkbox) {
+                return cellValue;
+            }
+        }
         return null;
     }
 }
