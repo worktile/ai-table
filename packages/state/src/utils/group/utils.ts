87a4527a2a562f56848527a0e3c78249d434b7b9
xwatson <xwatson@foxmail.com>
2025-09-01 11:15:38 +0800

    fix: getParentLinearRowGroups遇到空行逻辑错误

--- packages/state/src/utils/group/utils.ts
+++ packages/state/src/utils/group/utils.ts
@@ -1,24 +1,35 @@
 import { AITableLinearRowGroup, AITableRowType } from '@ai-table/grid';
 import { AIViewTable } from '../../types';
 
-export function getParentLinearRowGroups(aiTable: AIViewTable, groupId: string) {
-    const parentGroups: AITableLinearRowGroup[] = [];
+export function getParentLinearRowGroups(aiTable: AIViewTable, targetId: string) {
     const linearRows = aiTable.context!.linearRows();
-    let groupIndex = linearRows.findIndex((row) => row._id === groupId);
-    if (groupIndex > -1) {
-        const currentRow = linearRows[groupIndex];
-        let isBreak = false;
-        while (!isBreak) {
-            const groupRow = linearRows[groupIndex];
-            if (groupRow.depth === currentRow.depth || groupRow.type === AITableRowType.group) {
-                if (groupRow.type === AITableRowType.group) {
-                    parentGroups.push(groupRow);
-                }
-                groupIndex--;
-            } else {
-                isBreak = true;
+    const targetIndex = linearRows.findIndex((row) => row._id === targetId);
+
+    if (targetIndex === -1) {
+        return [];
+    }
+
+    const targetRow = linearRows[targetIndex];
+    const maxParentDepth = targetRow.depth || 0;
+
+    // 没有需要查找的父级深度，直接返回
+    if (maxParentDepth < 0) {
+        return [];
+    }
+
+    const parentGroups: AITableLinearRowGroup[] = [];
+    for (let i = targetIndex - 1; i >= 0 && parentGroups.length <= maxParentDepth; i--) {
+        const row = linearRows[i];
+
+        if (row.type === AITableRowType.group) {
+            const rowDepth = row.depth || 0;
+
+            // 如果是需要的父级深度，且还没有找到该深度的分组
+            if (rowDepth <= maxParentDepth && !parentGroups.some((g) => (g.depth || 0) === rowDepth)) {
+                parentGroups.push(row);
             }
         }
     }
+
     return parentGroups;
 }
