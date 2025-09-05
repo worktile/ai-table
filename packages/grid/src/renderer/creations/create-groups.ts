71dd81f71e52859570bee249b90da5d34024f2f0
icezeros <icezeros@163.com>
2025-09-05 10:29:12 +0800

    fix: #WIK-18928 有些分组没有渲染展开收起按钮

--- packages/grid/src/renderer/creations/create-groups.ts
+++ packages/grid/src/renderer/creations/create-groups.ts
@@ -1,27 +1,14 @@
-import {
-    AI_TABLE_CELL_PADDING,
-    AI_TABLE_FIELD_HEAD_ICON_GAP_SIZE,
-    AI_TABLE_FIELD_STAT_INNER_HEIGHT,
-    AI_TABLE_OFFSET,
-    Colors
-} from '../../constants';
-import {
-    AITableFieldStatsConfig,
-    AITableFieldStatConfig,
-    AITableGroupOptionsConfig,
-    AITableGroupConfig,
-    AITableRowType,
-    AITableCellsConfig
-} from '../../types';
+import { AI_TABLE_CELL_PADDING, AI_TABLE_OFFSET } from '../../constants';
+import { AITableGroupConfig, AITableRowType, AITableCellsConfig } from '../../types';
 import { getCellHorizontalPosition } from '../../utils';
 
 export const createGroupCells = (config: AITableCellsConfig) => {
     const { coordinate, rowStartIndex, rowStopIndex, columnStartIndex, columnStopIndex, aiTable, actions, readonly } = config;
     const linearRows = aiTable.context?.linearRows()!;
-    const { columnCount } = coordinate;
+    const { rowCount, columnCount } = coordinate;
     const groups: AITableGroupConfig[] = [];
     for (let rowIndex = rowStartIndex; rowIndex <= rowStopIndex; rowIndex++) {
-        if (rowIndex > columnCount - 1) break;
+        if (rowIndex > rowCount - 1) break;
         if (rowIndex < 0) continue;
         const row = linearRows[rowIndex];
         const { depth } = row;
