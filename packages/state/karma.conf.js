dd37ba20d34ec64a19d5770d1e21836288f499c2
Xwatson <xwatson@foxmail.com>
2025-09-24 11:47:03 +0800

    feat(state): 添加 sort 单元测试 (#663)

--- packages/state/karma.conf.js
+++ packages/state/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ai-table'),
+      dir: require('path').join(__dirname, '../../coverage/state'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ packages/state/src/test.ts
@@ -0,0 +1,6 @@
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+import './testing/utils/record/sort.spec';
