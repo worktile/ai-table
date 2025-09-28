b28d38bd9c65fd075e4f3b291b5a7dbfcc3a2812
pubuzhixing8 <pubuzhixing@gmail.com>
2025-09-28 17:49:02 +0800

    feat(action): support reset all records position on precision is not enough #WIK-19122

--- packages/state/src/action/position.ts
+++ packages/state/src/action/position.ts
@@ -1,5 +1,14 @@
 import { AIViewTable } from '../types';
-import { ActionName, NumberPath, Positions, RemovePositions, SetRecordPositionAction } from '@ai-table/utils';
+import {
+    ActionName,
+    AITableAction,
+    AITableViewRecord,
+    NumberPath,
+    Positions,
+    RemovePositions,
+    SetRecordPositionAction
+} from '@ai-table/utils';
+import { insertAtEnd, sortRecordsByConditions } from '../utils';
 
 export function buildSetRecordPositionsActon(aiTable: AIViewTable, positions: Positions | RemovePositions, path: NumberPath) {
     const action: SetRecordPositionAction = {
@@ -10,11 +19,29 @@ export function buildSetRecordPositionsActon(aiTable: AIViewTable, positions: Po
     return action;
 }
 
+export function resetAllRecordsPositions(aiTable: AIViewTable) {
+    const activeViewId = aiTable.activeViewId();
+    const activeView = aiTable.views().find((view) => view._id === activeViewId);
+    const records = aiTable.records() as AITableViewRecord[];
+    const recordsIndexMap = new Map(records.map((item, index) => [item._id, index]));
+    const sortedRecords = sortRecordsByConditions(aiTable, records, activeView!, []);
+    const actions: AITableAction[] = [];
+    const positions = insertAtEnd(0, sortedRecords.length);
+    sortedRecords.forEach((record, index) => {
+        const action = buildSetRecordPositionsActon(aiTable, { [activeView!._id]: positions[index].position }, [
+            recordsIndexMap.get(record._id)!
+        ]);
+        actions.push(action);
+    });
+    aiTable.apply(actions);
+}
+
 export function setRecordPositions(aiTable: AIViewTable, positions: Positions | RemovePositions, path: NumberPath) {
     const operation = buildSetRecordPositionsActon(aiTable, positions, path);
     aiTable.apply(operation);
 }
 
 export const PositionsActions = {
-    setRecordPositions
+    setRecordPositions,
+    resetAllRecordsPositions
 };
