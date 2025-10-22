1537e6c8fc245ce620ab54e9a1f45bebe8bdb3bb
pubuzhixing8 <pubuzhixing@gmail.com>
2025-10-22 15:43:06 +0800

    feat: handle positions when items is empty #WIK-19159

--- packages/state/src/utils/position-in-view.ts
+++ packages/state/src/utils/position-in-view.ts
@@ -103,12 +103,13 @@ export function getCurrentViewPositions<T extends AITableViewRecord | AITableVie
     items: T[],
     itemsMap: { [key: string]: T extends AITableViewRecord ? AITableRecord : AITableViewField }
 ) {
-    if (items.length === 0) {
-        return [DEFAULT_INITIAL_GAP];
-    }
     const { previousPosition, nextPosition } = getPreviousAndNextPosition(aiTable, options, items, itemsMap);
     const count = options.count || 1;
     let positions = [];
+    if (items.length === 0) {
+        positions = insertAtEnd(0, count);
+        return positions;
+    }
     if (options.beforeItemId && previousPosition === null && nextPosition !== null) {
         positions = insertAtStart(nextPosition, count);
     } else if (options.afterItemId && nextPosition === null && previousPosition !== null) {
