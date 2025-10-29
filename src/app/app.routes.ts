1ed73a7d8577e18dcd7810a312b6c5b35b3d00f1
minlovehua <321512939@qq.com>
2025-10-29 19:44:46 +0800

    refactor: organize group, filter, sort examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { TableBasicExample } from './examples/basic/basic.component';
 import { TableEditableExample } from './examples/editable/editable.component';
 import { TableFieldMenuExample } from './examples/field-menu/field-menu.component';
 import { TableViewExample } from './examples/view/view.component';
+import { TableGroupExample } from './examples/group/group.component';
+import { TableFilterExample } from './examples/filter/filter.component';
+import { TableSortExample } from './examples/sort/sort.component';
 
 export const routes: Routes = [
     {
@@ -37,5 +40,17 @@ export const routes: Routes = [
     {
         path: 'view',
         component: TableViewExample
+    },
+    {
+        path: 'filter',
+        component: TableFilterExample
+    },
+    {
+        path: 'sort',
+        component: TableSortExample
+    },
+    {
+        path: 'group',
+        component: TableGroupExample
     }
 ];
---
+++ src/app/examples/group/group.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-table-group-example',
+    templateUrl: './group.component.html'
+})
+export class TableGroupExample {}
