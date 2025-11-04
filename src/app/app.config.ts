feb0b0f0151f17a4287975a75c4055da2c3ac0a1
wumeimin0107 <321512939@qq.com>
2025-11-04 18:36:16 +0800

    refactor: adjust cover render to adaptive row height for custom field example (#770)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,16 +7,19 @@ import { provideRouter } from '@angular/router';
 import { ThyTooltipModule } from 'ngx-tethys/tooltip';
 import { routes } from './app.routes';
 import { THY_NOTIFY_DEFAULT_CONFIG_PROVIDER } from 'ngx-tethys/notify';
+import { THY_MESSAGE_DEFAULT_CONFIG_PROVIDER } from 'ngx-tethys/message';
 import { UndoManagerService } from '@ai-table/state';
+import { ThyPopoverModule } from 'ngx-tethys/popover';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        importProvidersFrom(BrowserModule, FormsModule, ThyTooltipModule),
+        importProvidersFrom(BrowserModule, FormsModule, ThyTooltipModule, ThyPopoverModule),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideHttpClient(),
         provideAnimations(),
         provideRouter(routes),
         THY_NOTIFY_DEFAULT_CONFIG_PROVIDER,
+        THY_MESSAGE_DEFAULT_CONFIG_PROVIDER,
         UndoManagerService
     ]
 };
