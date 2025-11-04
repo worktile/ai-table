5abc3f320fc5922030b48488c306e4f5d42c47cf
Xwatson <xwatson@foxmail.com>
2025-11-04 15:15:32 +0800

    feat: #WIK-19219 文本、选择、数字、日期、链接dom的多行编辑 (#764)

--- packages/grid/src/components/cell-editors/text/text-editor.component.ts
+++ packages/grid/src/components/cell-editors/text/text-editor.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { ThyInputDirective } from 'ngx-tethys/input';
 import { ThyAutofocusDirective, ThyEnterDirective } from 'ngx-tethys/shared';
 import { AbstractEditCellEditor } from '../abstract-cell-editor.component';
+import { ROW_HEIGHT_LEVELS } from '../../../constants';
 
 @Component({
     selector: 'text-cell-editor',
@@ -27,7 +28,6 @@ import { AbstractEditCellEditor } from '../abstract-cell-editor.component';
 })
 export class TextCellEditorComponent extends AbstractEditCellEditor<string> implements AfterViewInit {
     private render2 = inject(Renderer2);
-    private maxHeight = 148;
 
     private minHeight = 24;
 
@@ -48,10 +48,10 @@ export class TextCellEditorComponent extends AbstractEditCellEditor<string> impl
         const textarea = this.elementRef.nativeElement.querySelector('textarea');
         if (textarea) {
             this.render2.setStyle(textarea, 'height', 'auto');
-            const scrollHeight = textarea.scrollHeight;
-            const newHeight = Math.max(this.minHeight, Math.min(scrollHeight, this.maxHeight)) + 4;
+            const scrollHeight = Math.max(textarea.scrollHeight, this.rowHeight());
+            const newHeight = Math.max(this.minHeight, Math.min(scrollHeight, ROW_HEIGHT_LEVELS.high));
 
-            this.render2.setStyle(textarea, 'max-height', `${this.maxHeight}px`);
+            this.render2.setStyle(textarea, 'max-height', `${ROW_HEIGHT_LEVELS.high}px`);
             this.render2.setStyle(textarea, 'height', `${newHeight}px`);
             this.render2.setStyle(textarea, 'resize', 'none');
         }
--- packages/grid/src/services/event.service.ts
+++ packages/grid/src/services/event.service.ts
@@ -186,7 +186,8 @@ export class AITableGridEventService {
                 recordId: recordId,
                 references,
                 aiTable: aiTable,
-                isSelectAll: options.isSelectAll
+                isSelectAll: options.isSelectAll,
+                rowHeight: offsetOriginPosition.height
             },
             panelClass: 'grid-cell-editor',
             outsideClosable: fieldType === AITableFieldType.link ? true : false,
