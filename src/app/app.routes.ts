d027ec9a4f2bc9cf66141a72eaad94b03b11d402
minlovehua <321512939@qq.com>
2025-11-05 16:27:39 +0800

    refactor: add sort records example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { TableFieldMenuExample } from './examples/field-menu/field-menu.componen
 import { TableViewExample } from './examples/view/view.component';
 import { TableGroupExample } from './examples/group/group.component';
 import { TableFilterExample } from './examples/filter/filter.component';
-import { TableSortExample } from './examples/sort/sort.component';
+import { TableSortRecordsExample } from './examples/sort-records/sort-records.component';
 import { TableCustomFieldExample } from './examples/custom-field/custom-field.component';
 
 export const routes: Routes = [
@@ -51,8 +51,8 @@ export const routes: Routes = [
         component: TableFilterExample
     },
     {
-        path: 'sort',
-        component: TableSortExample
+        path: 'sort-records',
+        component: TableSortRecordsExample
     },
     {
         path: 'group',
