5dd59584f3ef2e603c782e316b48032dffa30504
icezeros <icezeros@163.com>
2025-12-05 15:18:52 +0800

    fix: #WIK-19476 【评分、进度】评分、进度在第一列时，评分和进度的交互处理

--- packages/grid/src/renderer/components/expand-record.component.ts
+++ packages/grid/src/renderer/components/expand-record.component.ts
@@ -1,31 +1,57 @@
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { KoContainer } from '../../angular-konva';
 import { AI_TABLE_OFFSET, ExpandRecordPath, Colors } from '../../constants';
-import { AITableActionIconConfig, AITableExpandRecordConfig, AITableRowType } from '../../types';
+import { AITableActionIconConfig, AITableBackgroundConfig, AITableExpandRecordConfig, AITableRowType } from '../../types';
 import { generateTargetName } from '../../utils';
 import {
     AI_TABLE_ACTION_COMMON_RADIUS,
     AI_TABLE_ACTION_COMMON_SIZE,
+    AI_TABLE_CELL_LINE_BORDER,
     AI_TABLE_EXPAND_RECORD_ICON,
     AI_TABLE_FIELD_HEAD_HEIGHT,
     AI_TABLE_ROW_HEAD_EXPAND_WIDTH
 } from '../../constants/table';
 import { AITableActionIcon } from './action-icon.component';
+import { AITableBackground } from './background.component';
+import { AITableFieldType } from '../../../../utils/src';
 
 @Component({
     selector: 'ai-table-expand-record',
     template: `
         <ko-group>
             @if (shouldShowIcon()) {
+                <ai-table-background [config]="backgroundConfig()"></ai-table-background>
                 <ai-table-action-icon [config]="expandIconConfig()"></ai-table-action-icon>
             }
         </ko-group>
     `,
-    imports: [KoContainer, AITableActionIcon],
+    imports: [KoContainer, AITableActionIcon, AITableBackground],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AITableExpandRecord {
     config = input.required<AITableExpandRecordConfig>();
+
+    backgroundConfig = computed<AITableBackgroundConfig>(() => {
+        const { coordinate, aiTable } = this.config()!;
+        const context = aiTable.context!;
+        const { rowIndex: pointRowIndex, columnIndex } = context.pointPosition();
+        const firstColumnOffset = coordinate.getColumnOffset(0);
+        const y = coordinate.getRowOffset(pointRowIndex) + AI_TABLE_OFFSET;
+        const rowHeight = coordinate.getRowHeight(pointRowIndex);
+        const firstField = aiTable.gridData().fields[0];
+        const isRateOrProgress = firstField?.type === AITableFieldType.rate || firstField?.type === AITableFieldType.progress;
+        const isFirstColumn = columnIndex === 0;
+        return {
+            coordinate,
+            x: firstColumnOffset - AI_TABLE_ROW_HEAD_EXPAND_WIDTH,
+            y: y,
+            width: AI_TABLE_ROW_HEAD_EXPAND_WIDTH + AI_TABLE_CELL_LINE_BORDER * 2 + AI_TABLE_OFFSET,
+            height: rowHeight,
+            fill: isRateOrProgress && isFirstColumn ? Colors.white : Colors.transparent,
+            hoverFill: Colors.transparent,
+            listening: true
+        };
+    });
 
     shouldShowIcon = computed(() => {
         const { aiTable, rowStartIndex, rowStopIndex } = this.config();
